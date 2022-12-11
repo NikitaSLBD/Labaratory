bac91fa6a3dc7ddd2cb2e2e149e27b8c22e51223
NikitaSLBD <nikitasloboda14@gmail.com>
2022-12-11 13:25:05 +0300

    Add RRPIIS.cpp & .exe

---
+++ RRPIIS.cpp
@@ -0,0 +1,19 @@
+﻿#include <cstdio>
+#include <set>
+
+using namespace std;
+
+struct Graph 
+{
+	int G_size;
+	set <int> Vertices;
+	set <float> Ribs;	
+};
+
+int main() {
+	Graph G1, G2;
+
+	system("pause");
+}
+
+
